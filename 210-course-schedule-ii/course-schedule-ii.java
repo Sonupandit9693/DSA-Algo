e6cf7c5d8282ed7eb003f079647fc0f77b404ca2
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-01-29 23:58:27 +0530

    Time: 6 ms (53.51%) | Memory: 46 MB (60.89%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,77 +1,49 @@
 class Solution {
-    static class DetectCycleGraph {
-		private int V;
-		private List<List<Integer>> edges;
-
-		DetectCycleGraph(int n) {
-			this.V = n;
-			System.out.println(n);
-			edges = new ArrayList<>();
-
-			for (int i = 0; i < n; i++) {
-				edges.add(new ArrayList<>());
-			}
-		}
-
-		public DetectCycleGraph() {
-		}
-
-		private void addEdge(int i, int j) {
-			edges.get(i).add(j);
-		}
-
-		List<Integer> l = new ArrayList<>();
-
-		public int[] isCycle(DetectCycleGraph graph) {
-
-			boolean[] visited = new boolean[this.V];
-			boolean[] curRec = new boolean[this.V];
-			for (int i = 0; i < this.V; i++) {
-				if (isCycleUtil(visited, curRec, i))
-					return new int[0];
-			}
-
-			int[] res = new int[V];
-			int index = 0;
-			for (Integer i : l) {
-				res[index] = i;
-				index++;
-			}
-
-			return res;
-		}
-
-		public boolean isCycleUtil(boolean[] visited, boolean[] curRec, int i) {
-
-			if (visited[i] == false) {
-
-				visited[i] = true;
-				curRec[i] = true;
-
-				List<Integer> neighbours = edges.get(i);
-				for (Integer v : neighbours) {
-
-					if (!visited[v] && isCycleUtil(visited, curRec, v))
-						return true;
-					else if (curRec[v])
-						return true;
-				}
-				l.add(new Integer(i));
-			}
-
-			curRec[i] = false;
-
-			return false;
-		}
-	}
-
-
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        DetectCycleGraph graph = new DetectCycleGraph(numCourses);
-		for (int i = 0; i < prerequisites.length; i++) {
-			int[] sub = prerequisites[i];
-			graph.addEdge(sub[0], sub[1]);
-		}
-		return graph.isCycle(graph);   
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for(int i=0; i < numCourses; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        int m = prerequisites.length;
+        for(int i=0; i<m; i++){
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+        int indegree [] = new int[numCourses];
+        for(int i=0; i < numCourses; i++){
+            for(int it : adj.get(i)){
+                indegree[it] ++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<Integer>();
+        for(int i=0; i < numCourses; i++){
+            if(indegree[i] == 0){
+                q.add(i);
+            }
+        }
+
+        int topo [] = new int[numCourses];
+        int ind = 0;
+
+        while(! q.isEmpty()){
+            int node = q.peek();
+            q.remove();
+
+            topo[ind ++] = node;
+
+            for(int it : adj.get(node)){
+                indegree[it] --;
+                if(indegree[it] == 0){
+                    q.add(it);
+                }
+            }
+        }
+
+        if(ind == numCourses) return topo;
+        int arr [] = {};
+        return arr;
     }
 }
