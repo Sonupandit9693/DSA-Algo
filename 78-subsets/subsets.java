85849b38b1851d8a9b94a7e5e8a6f8cddc89162c
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-03-05 23:29:29 +0530

    Time: 1 ms (53.97%) | Memory: 42.8 MB (46.26%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+        genrateSubsets(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void genrateSubsets(int index, int[] nums, List<Integer> current, List<List<Integer>> result){
+        result.add(new ArrayList<>(current));
+        for(int i = index; i< nums.length; i++){
+            current.add(nums[i]);
+            genrateSubsets(i + 1, nums, current, result);
+
+            // backtrack value are remove
+            current.remove(current.size() - 1);
+        }
+    }
+}
