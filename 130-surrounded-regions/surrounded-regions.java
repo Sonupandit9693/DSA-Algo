b3e6e26c68dd3feff9422ad8340e2bcd45ec6b59
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-03-12 22:13:48 +0530

    Time: 2 ms (86.19%) | Memory: 45.3 MB (84.76%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,63 +1,53 @@
 class Solution {
 
-    public void dfs(int row, int col, char[][] board, int [][] vis, int[] delrow, int [] delcol){
-        vis[row][col] = 1;
-        int n = board.length;
-        int m = board[0].length;
+    private static final int[] delrow = {-1, 0, 1, 0};
+    private static final int[] delcol = {0, 1, 0, -1};
+    public void solve(char[][] board) {
+        int n = board.length, m = board[0].length;
 
-        for(int i=0; i<4; i++){
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
+        // start dfs from 'O' from the boundry and mark them True
 
-            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 && board[nrow][ncol] == 'O'){
-                vis[nrow][ncol] = 1;
-                dfs(nrow, ncol, board, vis, delrow, delcol);
+        for(int i=0; i<n; i++){
+            if(board[i][0] == 'O'){
+                dfs(i, 0, board);
+            }
+
+            if(board[i][m-1] == 'O'){
+                dfs(i, m-1, board);
+            }
+        }
+
+        for(int j=0; j<m; j++){
+            if(board[0][j] == 'O'){
+                dfs(0, j, board);
+            }
+            if(board[n-1][j] == 'O'){
+                dfs(n-1, j, board);
+            }
+        }
+
+
+        //convert remaing 'O' to 'X' (captures regions)
+        // convert True back to False (boundry connected regions)
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j] == 'O') board[i][j] = 'X';
+                if (board[i][j] == 'T') board[i][j] = 'O';
             }
         }
     }
 
-    public void solve(char[][] board) {
-        int delrow[] = {-1, 0, +1, 0};
-        int delcol[] = {0, 1, 0, -1}; 
+    private void dfs(int row, int col, char[][] board){
+        int n = board.length, m = board[0].length;
 
-        int n = board.length;
-        int m = board[0].length;
-
-        int [][] vis = new int[n][m];
-
-        for(int j=0; j<m; j++){
-            // check first row of boundry
-
-            if(board[0][j] == 'O' && vis[0][j] == 0){
-                dfs(0, j, board, vis, delrow, delcol);
-            }
-
-            // last row of boundry
-            if(board[n-1][j] == 'O' && vis[n-1][j] == 0){
-                dfs(n-1, j, board, vis, delrow, delcol);
-            }
+        if(row < 0 || col < 0 || row >= n || col >= m || board[row][col] != 'O'){
+            return;
         }
 
-        for(int i=0; i<n; i++){
-            // first column
-            if(board[i][0] == 'O' && vis[i][0] == 0){
-                dfs(i, 0, board, vis, delrow, delcol);
-            }
-
-            // last column
-            if(board[i][m-1] == 'O' && vis[i][m-1] == 0){
-                dfs(i, m-1, board, vis, delrow, delcol);
-            }
+        board[row][col]= 'T';
+        for(int i=0; i<4; i++){
+            dfs(row + delrow[i], col + delcol[i], board);
         }
-
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(board[i][j] == 'O' && vis[i][j] == 0){
-                    board[i][j] = 'X';
-                }
-            }
-        }
-
     }
 }
