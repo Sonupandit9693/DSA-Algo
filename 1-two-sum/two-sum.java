155abb9cac1108b5ca04c6a8ac62d1e8510a50a0
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-03-19 23:35:48 +0530

    Time: 2 ms (98.84%) | Memory: 44.4 MB (99.43%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,15 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i < nums.length; i++){
-            int complement = target - nums[i];
+        // create map to validate complement of tagrget
+        Map<Integer, Integer> map = new HashMap<>(); // store the indes
+
+        // iterate on nums arry
+        for(int i=0; i<nums.length; i++){
+            int complement = target - nums[i]; // find next value
             if(map.containsKey(complement)){
-                return new int [] {map.get(complement), i};
+                return new int[] {map.get(complement), i};
             }
 
             map.put(nums[i], i);
         }
 
-        return new int[] { -1, -1};
+        // if not find then return {-1, -1}
+        return new int[] {-1, -1};
     }
 }
