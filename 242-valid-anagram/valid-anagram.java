c1dd9848c03c29420c2990d5b957afe0fe18603b
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-03-19 23:26:46 +0530

    Time: 5 ms (53.72%) | Memory: 42.7 MB (97.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        // first check if both string length not eqal then not anagram return false
         if(s.length() != t.length()){
             return false;
         }
 
-        int characterCount[] = new int[26];
+        // crate charter array to mange count
+        int charAnagram[] = new int[26]; // only have 26 alaphabets
 
-        for(int i=0; i<s.length(); i++){
-            characterCount[s.charAt(i) - 'a'] ++;
-            characterCount[t.charAt(i) - 'a'] --;
+        for(int i=0; i < s.length(); i++){
+            charAnagram[s.charAt(i) - 'a'] ++;
+            charAnagram[t.charAt(i) - 'a'] --;
         }
 
-        for( int c : characterCount){
+        for(int c: charAnagram){
             if(c != 0){
                 return false;
             }
