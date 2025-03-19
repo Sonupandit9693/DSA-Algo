599ab7cc5adde029c6201d40c533e2c8689fcec8
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-03-20 00:03:52 +0530

    Time: 6 ms (97.87%) | Memory: 47.7 MB (82.51%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,37 +1,24 @@
 class Solution {
+    private HashMap<String, List<String>> ans = new HashMap<>();
     public List<List<String>> groupAnagrams(String[] strs) {
-        // check if strs is empty list
         if(strs.length == 0){
             return new ArrayList<>();
+        }else{
+            for(int i=0; i<strs.length; i++){
+                char chars[] = strs[i].toCharArray();
+                Arrays.sort(chars);
+                String key = new String(chars);
+                if(ans.containsKey(key)){
+                    List<String> list1 = ans.get(key);
+                    list1.add(strs[i]);
+                }else{
+                    List<String> list2 = new ArrayList<>();
+                    list2.add(strs[i]);
+                    ans.put(key, list2);
+                }
+            }
         }
 
-        HashMap<String, List> ansMap = new HashMap<>();
-
-        // create charater array of size 26
-        int count [] = new int [26];
-
-        for(String s: strs){
-            // intialize count arrya value with 0
-            Arrays.fill(count, 0);
-            for(int c: s.toCharArray()){
-                count[c - 'a'] ++;
-            }
-
-            // create hashkey for match group of nanagram
-            StringBuilder sb = new StringBuilder("");
-            // go for 26 charcter to genrate hashKey
-            for(int i=0; i<26; i++){
-                sb.append("#");
-                sb.append(count[i]);
-            }
-
-            String key = sb.toString();
-            if(! ansMap.containsKey(key)){
-                ansMap.put(key, new ArrayList<>());
-            }
-            ansMap.get(key).add(s);
-        }
-
-        return new ArrayList(ansMap.values());
+        return new ArrayList(ans.values());
     }
 }
