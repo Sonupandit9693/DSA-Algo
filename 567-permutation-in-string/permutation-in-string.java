fee7877be742aff07388d7b04486a9886ab1afa6
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-04-02 23:04:41 +0530

    Time: 7 ms (72.88%) | Memory: 42.5 MB (95.27%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -4,33 +4,35 @@ class Solution {
             return false;
         }
 
-        int s1map[] = new int [26];
-        int s2map[] = new int [26];
+        int s1map[] = new int[26];
+        int s2map[] = new int[26];
 
-        for(int i=0; i<s1.length(); i++){
-            s1map[s1.charAt(i) - 'a'] ++;
+        for(int i=0; i < s1.length(); i++){
+            s1map[s1.charAt(i) - 'a']++;
             s2map[s2.charAt(i) - 'a'] ++;
         }
 
-        // compare both map with help of slide window
-        for(int i=0; i< s2.length() - s1.length(); i++){
+        for(int i=0; i < s2.length() - s1.length(); i++){
             if(match(s1map, s2map)){
                 return true;
             }
-            s2map[s2.charAt(i + s1.length()) - 'a'] ++; // add new Elenent in window;
-            s2map[s2.charAt(i) - 'a'] --; // remove previous window element
+
+            // add new element in window
+            s2map[s2.charAt(i + s1.length()) - 'a'] ++;
+            // remove previos window last element
+            s2map[s2.charAt(i) - 'a'] --;
         }
 
         return match(s1map, s2map);
     }
 
     public boolean match(int s1map[], int s2map[]){
-        for(int i=0; i< 26; i++){
+        for(int i=0; i<26; i++){
             if(s1map[i] != s2map[i]){
                 return false;
             }
         }
-        return true;
 
+        return true;
     }
 }
