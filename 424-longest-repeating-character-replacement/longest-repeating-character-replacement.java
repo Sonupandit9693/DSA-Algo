215b3d6c4442d835c5ab589c07d730233e1ecfbf
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-04-02 22:42:05 +0530

    Time: 8 ms (84.18%) | Memory: 45.3 MB (26.59%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,18 +1,17 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] occurrences = new int[26];
+        int occurences[] = new int [26];
+        int maxOccurences = 0;
         int left = 0;
-        int maxOccurrences = 0;
         int ans = 0;
 
-        for (int right = 0; right < s.length(); right++) {
-            occurrences[s.charAt(right) - 'A']++;
-            maxOccurrences = Math.max(maxOccurrences, occurrences[s.charAt(right) - 'A']);
+        for(int right=0; right < s.length(); right ++){
+            occurences[s.charAt(right) - 'A'] ++;
+            maxOccurences = Math.max(maxOccurences, occurences[s.charAt(right) - 'A']);
 
-            // Check if the remaining characters (window size - maxOccurrences) exceed k
-            while (right - left + 1 - maxOccurrences > k) {
-                occurrences[s.charAt(left) - 'A']--;
-                left++;
+            while(right - left + 1 - maxOccurences > k){
+                occurences[s.charAt(left) - 'A'] --;
+                left ++;
             }
 
             ans = Math.max(ans, right - left + 1);
