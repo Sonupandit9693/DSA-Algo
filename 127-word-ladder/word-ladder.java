98b376128f03024d442dfb6235b527c5256c7fc3
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-04-11 23:05:13 +0530

    Time: 96 ms (38.97%) | Memory: 47 MB (44.28%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,40 +1,43 @@
-class Pair{
-    String word;
-    int step;
-    Pair(String word, int step){
-        this.word = word;
-        this.step = step;
-    }
-}
-
 class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(beginWord, 1));
+    // creating Pair for to have atribute word and number of steps
+    class Pair{
+        String word;
+        int step;
+        Pair(String word, int step){
+            this.word = word;
+            this.step = step;
+        }
+    }
 
-        Set<String> st = new HashSet<String>();
-        int len = wordList.size();
-        for(int i=0; i<len; i++){
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        // i use BFS to check Sortest path of step
+        Queue<Pair> queue = new LinkedList<>();
+        // add beging word in queue
+        queue.add(new Pair(beginWord, 1));
+
+        // creating a Hashset to manage alredy process word of wordlist
+        Set<String> st = new HashSet<>();
+        int n = wordList.size();
+        for(int i=0; i < n; i++){
             st.add(wordList.get(i));
         }
-
         st.remove(beginWord);
 
-        while(! q.isEmpty()){
-            String word = q.peek().word;
-            int step = q.peek().step;
-            q.remove();
+        while(! queue.isEmpty()){
+            String word = queue.peek().word;
+            int step = queue.peek().step;
+            queue.remove();
 
             if(word.equals(endWord) == true) return step;
 
-            for(int i=0; i< word.length(); i++){
-                for(char ch ='a'; ch <= 'z'; ch++){
-                    char replacedArray [] = word.toCharArray();
-                    replacedArray[i] = ch;
-                    String replacedWord = new String(replacedArray);
-                    if(st.contains(replacedWord) == true){
-                        st.remove(replacedWord);
-                        q.add(new Pair(replacedWord, step + 1));
+            for(int i=0; i<word.length(); i++){
+                for(char ch='a'; ch <= 'z'; ch ++){
+                    char replaceArray[] = word.toCharArray();
+                    replaceArray[i] = ch;
+                    String replaceWord = new String(replaceArray);
+                    if(st.contains(replaceWord) == true){
+                        st.remove(replaceWord);
+                        queue.add(new Pair(replaceWord, step+1));
                     }
                 }
             }
