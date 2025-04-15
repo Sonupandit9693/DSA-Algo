d2aa6c1427307b1c11f6bde081a95e9e8d2b1ec8
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-04-15 21:55:53 +0530

    Time: 26 ms (91.47%) | Memory: 51.4 MB (84.36%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,26 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        // sort the arry
         Arrays.sort(nums);
-        
+
+        // inotalize our result arraylist 
         List<List<Integer>> result = new ArrayList<>();
 
         for(int i=0; i<nums.length -2; i++){
-            if( i>0 && nums[i] == nums[i-1]) continue;
-
-            if(nums[i] > 0){
-                break;
+            // check duplicate
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
             }
 
-            int left = i +1;
-            int right = nums.length -1;
+            // incase get positive value i brek loop my ans not longer exits
+            if(nums[i] > 0){
+                break;
+            } 
+
+            int left = i + 1;
+            int right = nums.length - 1;
 
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
+
                 if(sum == 0){
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    result.add(Arrays.asList(nums[left], nums[right], nums[i]));
                     left ++;
                     right --;
 
+                    // also check duplicate
                     while(left < right && nums[left] == nums[left -1]) left ++;
                     while(left < right && nums[right] == nums[right + 1]) right --;
                 }else if(sum < 0){
@@ -28,8 +36,6 @@ class Solution {
                 }else{
                     right --;
                 }
-
-
             }
         }
 
