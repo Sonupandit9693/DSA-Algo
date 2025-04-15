03bb872a67cb8f52691e7a670add50b99e2637be
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-04-15 22:21:55 +0530

    Time: 5 ms (74.76%) | Memory: 58.1 MB (31.78%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -9,10 +9,15 @@ class Solution {
         int right = height.length -1;
 
         while(left < right){
+            // create width
             int width = right - left;
-            int area = Math.min(height[left],height[right]) * width;
+            // calculate area of to stre water
+            int area = Math.min(height[left], height[right]) * width;
+
+            // upadte our max value
             max = Math.max(max, area);
 
+            // upadte pointer
             if(height[left] <= height[right]){
                 left ++;
             }else{
