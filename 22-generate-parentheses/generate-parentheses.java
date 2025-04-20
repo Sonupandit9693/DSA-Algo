21eaa9dc7230fbf0e3b463aef790bbed43ee6eca
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-04-20 22:28:51 +0530

    Time: 2 ms (47.11%) | Memory: 43.3 MB (59.24%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,25 +1,27 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        generate(result, "", 0, 0, n);
+
+        genrateParentheses(result, "", 0, 0, n);
         return result;
     }
 
-    private void generate(List<String> result, String current, int open, int close, int max) {
-        // If the current string length equals the maximum possible length
-        if (current.length() == max * 2) {
-            result.add(current);
+    public void genrateParentheses(List<String> result, String currentString, int open, int close, int max){
+
+        // check if string completed
+        if(currentString.length() == max * 2){
+            result.add(currentString);
             return;
         }
-        
-        // Add an opening parenthesis if it's allowed
-        if (open < max) {
-            generate(result, current + "(", open + 1, close, max);
+
+        // add then open parentheses
+        if(open < max){
+            genrateParentheses(result, currentString + "(" , open + 1, close, max);
         }
-        
-        // Add a closing parenthesis if it's valid
-        if (close < open) {
-            generate(result, current + ")", open, close + 1, max);
+
+        // add close parentheses
+        if(close < open){
+            genrateParentheses(result, currentString + ")", open, close + 1, max);
         }
     }
 }
