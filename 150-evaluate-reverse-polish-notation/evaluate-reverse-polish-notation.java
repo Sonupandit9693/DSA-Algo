79ee12987375e82a5fc2c1906d9f208494886d3f
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-04-20 22:16:47 +0530

    Time: 6 ms (74.36%) | Memory: 44.6 MB (82.15%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -2,12 +2,12 @@ class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
 
-        for(String token : tokens){
-            if(isOpertor(token)){
+        for(String token: tokens){
+            if(isOperator(token)){
                 int b = stack.pop();
                 int a = stack.pop();
 
-                int result = calculateResult(token, a , b);
+                int result = calculateResult(token, a, b);
                 stack.push(result);
             }else{
                 stack.push(Integer.parseInt(token));
@@ -17,21 +17,17 @@ class Solution {
         return stack.pop();
     }
 
-    boolean isOpertor (String token){
+    boolean isOperator(String token){
         return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
     }
 
-    private int calculateResult(String token, int a, int b){
+    int calculateResult(String token, int a, int b){
         switch(token){
             case "+" : return a + b;
             case "-" : return a - b;
             case "*" : return a * b;
             case "/" : return a / b;
-            default : throw new IllegalArgumentException("Invalid Options");
+            default : throw new IllegalArgumentException("Invalid operrator");
         }
     }
 }
-
-
-
-// default : throw new IllegalArgumentException("Invalid options");
