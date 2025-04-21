b46014fd5d41ca661b1950fe1f783cd0b519d7f0
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-04-21 23:41:55 +0530

    Time: 25 ms (42.89%) | Memory: 45.6 MB (16.28%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -7,36 +7,24 @@ class Solution {
             high = Math.max(high, pile);
         }
 
-        // System.out.println("\nHeight of pliels: " + high);
-       
         while(low < high){
-            int mid = low + (high - low) / 2; // our Speed to the bannas
-            // System.out.println("low value: " +  low);
-            // System.out.println("high value: " +  high);
-            // System.out.println("mid: " +  mid);
-            if(canFinish(mid, piles, h)){
+            int mid = low + (high - low) / 2;
 
-                // search minimun speed so go left part
+            if(canFinish(mid, piles, h)){
                 high = mid;
             }else{
-                // other wise search on rigth that speed can finish then banna
                 low = mid + 1;
             }
-
-            
-
         }
 
         return low;
     }
 
-    private boolean canFinish(int speed, int piles[], int h){
+    boolean canFinish(int speed, int piles[], int h){
         int hour = 0;
 
         for(int pile : piles){
-            // System.out.println("ceil value is : " + Math.ceil((double) pile / speed));
-            hour += Math.ceil((double) pile / speed);;
-
+            hour += Math.ceil((double)pile / speed);
         }
 
         return hour <= h;
