b5a82db47b851c0a79a47f9e8a022efa505a27c0
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-04-21 23:09:52 +0530

    Time: 69 ms (61.31%) | Memory: 57.5 MB (63.64%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,34 +1,24 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        // we declear a stack to store bar 
-        Stack<Integer> stack = new Stack<>();
-
-        int max_area = 0;
+        // monotonic stack approach
         int n = heights.length;
-
+        Stack<Integer> stack = new Stack<>();
+        int max_area = 0;
         for(int i=0; i<=n; i++){
-            int currHeight = (i == n) ? 0: heights[i];
-            
-            // System.out.println("\nCurrent index: " + i + ", Height: " + currHeight);
-            // System.out.println("Stack before: " + stack);
+            int currHeight = (i == n ? 0 : heights[i]);
 
-            // check in stack peek element 
             while(! stack.isEmpty() && currHeight < heights[stack.peek()]){
-                int topIndex = stack.pop();
-                int height = heights[topIndex];
+                int top = stack.pop();
+                int height = heights[top];
                 int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+
                 int area = height * width;
                 max_area = Math.max(max_area, area);
-
-                // System.out.println("  -> Popped index: " + topIndex + ", height: " + height);
-                // System.out.println("     Width: " + width + ", Area: " + area);
-                // System.out.println("     Max Area so far: " + max_area);
             }
+
             stack.push(i);
-            // System.out.println("Stack after: " + stack);
         }
 
-        // System.out.println("\nFinal Max Area: " + max_area);
         return max_area;
     }
 }
