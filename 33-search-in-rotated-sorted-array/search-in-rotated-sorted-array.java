572299f73b29a30ce8b19491c2186a489a35da01
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-04-22 00:07:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (40.06%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -5,23 +5,25 @@ class Solution {
         }
 
         int left = 0;
-        int right = nums.length -1;
+        int right = nums.length - 1;
 
         while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] == target) return mid;
-            // check half part sorted
+            int mid = left+ (right-left)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+
             if(nums[left] <= nums[mid]){
                 // left part is sorted
                 if(nums[left] <= target && target < nums[mid]){
                     right = mid -1;
                 }else{
-                    left = mid + 1;
+                    left = mid +1;
                 }
             }else{
-                // right part is shorted
+                // check right part is sorted or not
                 if(nums[mid] < target && target <= nums[right]){
-                    left = mid + 1;
+                    left = mid+ 1;
                 }else{
                     right = mid - 1;
                 }
