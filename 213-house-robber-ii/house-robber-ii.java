cdcff15a44d1cec9fc0b9dce028ecf8ca04ed5ae
Sonu kumar <111635226+Sonupandit9693@users.noreply.github.com>
2025-05-07 22:58:44 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (80.46%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,19 +1,22 @@
-public class Solution {
-    
+class Solution {
     public int rob(int[] nums) {
-        return Math.max(nums[0], 
-               Math.max(helper(Arrays.copyOfRange(nums, 1, nums.length)), 
-               helper(Arrays.copyOfRange(nums, 0, nums.length - 1))));
+        int maxProduct = Math.max(
+            helper(Arrays.copyOfRange(nums, 1, nums.length)),
+            helper(Arrays.copyOfRange(nums, 0, nums.length -1))
+        );
+
+        return Math.max(nums[0], maxProduct);
     }
 
-    private int helper(int[] nums) {
-        int rob1 = 0, rob2 = 0;
+    private int helper(int[] nums){
+        int rob1=0, rob2=0;
 
-        for (int num : nums) {
-            int newRob = Math.max(rob1 + num, rob2);
+        for(int num: nums){
+            int maxRob = Math.max(num + rob1, rob2);
             rob1 = rob2;
-            rob2 = newRob;
+            rob2 = maxRob;
         }
+
         return rob2;
     }
 }
